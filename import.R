7273b51b9118cf11a6f377366b9c800c2302d2fa
deannabeatty <deanna.beatty@gmail.com>
2025-03-01 13:25:31 -0800

    update R scripts

--- import.R
+++ import.R
@@ -52,23 +52,23 @@ head(tax_80$data)
 tax_80 <- parse_taxonomy(tax_80$data)
 
 # remove rows with unassigned and add column with percent similarity blastn
-tax_99_red <- filter(tax_99, Kingdom != "Unassigned") # 397 entries
+tax_99_red <- filter(tax_99, Kingdom != "Unassigned")
 tax_99_red$Percent_similarity <- c("0.99")
-tax_99_red <- filter(tax_99_red, !is.na(Order)) # 367 entries (30 entries of Actinopteri, unresolved lower taxonomic levels)
+tax_99_red <- filter(tax_99_red, !is.na(Order))
 
-tax_97_red <- filter(tax_97, Kingdom != "Unassigned") # 468 entries
+tax_97_red <- filter(tax_97, Kingdom != "Unassigned")
 tax_97_red$Percent_similarity <- c("0.97")
-tax_97_red <- filter(tax_97_red, !is.na(Order)) # 443 entries (25 entries of Actinopteri, unresolved lower taxonomic levels)
+tax_97_red <- filter(tax_97_red, !is.na(Order))
 
-tax_95_red <- filter(tax_95, Kingdom != "Unassigned") # 509 entries
+tax_95_red <- filter(tax_95, Kingdom != "Unassigned")
 tax_95_red$Percent_similarity <- c("0.95")
-tax_95_red <- filter(tax_95_red, !is.na(Order)) # 487 entries (22 entries of Actinopteri, unresolved lower taxonomic levels)
+tax_95_red <- filter(tax_95_red, !is.na(Order))
 
-tax_90_red <- filter(tax_90, Kingdom != "Unassigned") # 601 entries
+tax_90_red <- filter(tax_90, Kingdom != "Unassigned")
 tax_90_red$Percent_similarity <- c("0.90")
-tax_90_red <- filter(tax_90_red, !is.na(Order)) # 594 entries (7 entries of Actinopteri, unresolved lower taxonomic levels)
+tax_90_red <- filter(tax_90_red, !is.na(Order))
 
-tax_80_red <- filter(tax_80, Kingdom != "Unassigned") # 629 entries (31 entries of Actinopteri, unresolved lower taxonomic levels)
+tax_80_red <- filter(tax_80, Kingdom != "Unassigned")
 tax_80_red$Percent_similarity <- c("0.80")
 
 # get hash id for each table
@@ -80,63 +80,63 @@ hash_80 <- row.names(tax_80_red)
 
 # find intersection of 97 to 99 hash ids
 hash_intersect_97 <- intersect(hash_99, hash_97)
-length(hash_intersect_97) # 367
+length(hash_intersect_97)
 
 # remove intersect hash ids from 97 df
 tax_97_fill <- tax_97_red %>% filter(!row.names(tax_97_red) %in% hash_intersect_97)
-nrow(tax_97_fill) # 76
-nrow(tax_99_red) # 367
+nrow(tax_97_fill)
+nrow(tax_99_red)
 # bind rows
 tax_99_bind <- bind_rows(tax_99_red, tax_97_fill)
-nrow(tax_99_bind) # 443
+nrow(tax_99_bind)
 
 # hash id from tax_99_bind 
 hash_id_bind <- row.names(tax_99_bind)
-length(hash_id_bind) # 443
+length(hash_id_bind)
 
 # find intersection 95 to hash_id_bind 
 hash_intersect_95 <- intersect(hash_id_bind, hash_95)
-length(hash_intersect_95) # 443
+length(hash_intersect_95) 
 
 # remove intersect hash ids from 95 df
 tax_95_fill <- tax_95_red %>% filter(!row.names(tax_95_red) %in% hash_intersect_95)
-nrow(tax_95_fill) # 44
-nrow(tax_99_bind) # 443
+nrow(tax_95_fill) 
+nrow(tax_99_bind)
 # bind rows
 tax_99_bind <- bind_rows(tax_99_bind, tax_95_fill)
-nrow(tax_99_bind) # 487
+nrow(tax_99_bind)
 
 # hash id from tax_99_bind
 hash_id_bind <- row.names(tax_99_bind)
-length(hash_id_bind) # 487
+length(hash_id_bind)
 
 # find intersection 90 to hash_id_bind 
 hash_intersect_90 <- intersect(hash_id_bind, hash_90)
-length(hash_intersect_90) # 483
+length(hash_intersect_90)
 
 # remove intersect hash ids from 90 df
 tax_90_fill <- tax_90_red %>% filter(!row.names(tax_90_red) %in% hash_intersect_90)
-nrow(tax_90_fill) # 111
-nrow(tax_99_bind) # 487
+nrow(tax_90_fill)
+nrow(tax_99_bind)
 # bind rows
 tax_99_bind <- bind_rows(tax_99_bind, tax_90_fill)
-nrow(tax_99_bind) # 598
+nrow(tax_99_bind)
 
 # hash id from tax_99_bind
 hash_id_bind <- row.names(tax_99_bind)
-length(hash_id_bind) # 598
+length(hash_id_bind)
 
 # find intersection 80 to hash_id_bind 
 hash_intersect_80 <- intersect(hash_id_bind, hash_80)
-length(hash_intersect_80) # 598
+length(hash_intersect_80)
 
 # remove intersect hash ids from 80 df
 tax_80_fill <- tax_80_red %>% filter(!row.names(tax_80_red) %in% hash_intersect_80)
-nrow(tax_80_fill) # 31
-nrow(tax_99_bind) # 598
+nrow(tax_80_fill)
+nrow(tax_99_bind)
 # bind rows
 tax_99_bind <- bind_rows(tax_99_bind, tax_80_fill)
-nrow(tax_99_bind) # 629 (18 Actinopteri without lower taxonomic resolution/consensus at 0.80 similarity)
+nrow(tax_99_bind)
 
 # change column name to Domain
 colnames(tax_99_bind)[1] <- "Domain"
@@ -171,9 +171,11 @@ for(row in 1:nrow(df)) {
 
 # create column with genus species and percent similarity
 df$Species_percent_sim <- paste(df$Species, " ", df$Percent_similarity) 
-# one OTU of Bacteria in taxonomy was removed from count table during qiime2 filtering
 
 tax_99_bind <- df
+
+# filter to 0.97 or higher similarity 
+tax_only_97_sim_or_higher <- tax_99_bind %>% filter(Percent_similarity %in% c("0.97", "0.99"))
 
 # convert df to matrix
 tax_matrix <- as.matrix(tax_99_bind, rownames = TRUE, colnames = TRUE)
@@ -194,7 +196,7 @@ Fish_phyloseq_non_rarefied <- qza_to_phyloseq(
 
 # add tax_table to phyloseq object
 tax_table(Fish_phyloseq) <- tax_matrix
-view(tax_table(Fish_phyloseq)) # 614 OTUs remain in rarefied table from 629 total (98% of OTUs)
+view(tax_table(Fish_phyloseq))
 Fish_phyloseq # 175 samples
 tax_table(Fish_phyloseq_non_rarefied) <- tax_matrix
 view(tax_table(Fish_phyloseq_non_rarefied))
@@ -204,15 +206,15 @@ Fish_phyloseq_non_rarefied # 186 samples
 fish_2019 <- subset_samples(Fish_phyloseq, Year == "2019") # 91 samples (6 samples lost to rarefying, 2 neg control 0 counts, 1 drop out sample)
 fish_2020 <- subset_samples(Fish_phyloseq, Year == "2020") # 84 samples (5 samples lost to rarefying, 2 neg control 0 counts, 1 drop out sample)
 # prune OTUs from phyloseq object with zero counts across all samples within each dataset
-fish_2019_sp <- filter_taxa(fish_2019, function(x) sum(x) > 0, TRUE) # 461 OTUs & 91 samples 
-fish_2020_sp <- filter_taxa(fish_2020, function(x) sum(x) > 0, TRUE) # 337 OTUs  & 84 samples
+fish_2019_sp <- filter_taxa(fish_2019, function(x) sum(x) > 0, TRUE)
+fish_2020_sp <- filter_taxa(fish_2020, function(x) sum(x) > 0, TRUE)
 
 # filter by year non-rarefied
 fish_2019_not_rare <- subset_samples(Fish_phyloseq_non_rarefied, Year == "2019") 
 fish_2020_not_rare <- subset_samples(Fish_phyloseq_non_rarefied, Year == "2020")
 # prune OTUs from phyloseq object with zero counts across all samples within each dataset
-fish_2019_not_rare_sp <- filter_taxa(fish_2019_not_rare, function(x) sum(x) > 0, TRUE) # 474 OTUs & 97 samples
-fish_2020_not_rare_sp <- filter_taxa(fish_2020_not_rare, function(x) sum(x) > 0, TRUE) # 345 OTUs & 89 samples
+fish_2019_not_rare_sp <- filter_taxa(fish_2019_not_rare, function(x) sum(x) > 0, TRUE)
+fish_2020_not_rare_sp <- filter_taxa(fish_2020_not_rare, function(x) sum(x) > 0, TRUE)
 
 #add metadata to phyloseq object
 metadata_2019 <- metadata_2019 %>% column_to_rownames("sampleid")
@@ -248,3 +250,12 @@ Fish_phyloseq_nonrarefied_nonclustered <- qza_to_phyloseq(
 )
 saveRDS(Fish_phyloseq_nonrarefied_nonclustered, file = "output/phyloseq_object/Fish_phyloseq_nonrarefied_nonclustered.rds")
 view(tax_table(Fish_phyloseq_nonrarefied_nonclustered))
+
+# create phyloseq object on non-clustered but rarefied to 20000 with taxonomic assignments 97% similarity
+Fish_phyloseq_nonclustered_rarefied <- qza_to_phyloseq(
+  features = "output/table_2019_2020_dada2_r20000.qza",
+  taxonomy = "output/rep_seqs_2019_2020_dada2_taxonomy_12S_16S_18S_97.qza",
+  metadata = "metadata/meta_all.txt"
+)
+saveRDS(Fish_phyloseq_nonclustered_rarefied, file = "output/phyloseq_object/Fish_phyloseq_nonclustered_rarefied.rds")
+view(tax_table(Fish_phyloseq_nonclustered_rarefied))
